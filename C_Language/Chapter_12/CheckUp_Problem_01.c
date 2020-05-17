3f11f57a4a48230ec486b332da1c8bd1b48977f2
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-05-18 01:04:25 +0900

    2020.05.18_Study string

---
+++ C_Language/Chapter_12/CheckUp_Problem_01.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+// p.335
+
+int main(void)
+{
+
+	char* ps = "applepie";
+	ps += 5;
+	
+	printf("%s", ps);
+
+	return 0;
+
+}
---
+++ C_Language/Chapter_12/CheckUp_Problem_04.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+// p.353
+
+int main(void)
+{
+	char str[80]="A";
+	char strcp[80]="A";
+
+	printf("단어 입력: ");
+	scanf("%s", str);
+
+	if (strlen(str) <= 15) {
+	
+		strcpy(strcp, str, 5);
+
+		if (strlen(str) > 5) {
+			for (int i = 0; i < strlen(str) - 5; i++) 
+			{
+				strcat(strcp, "*");
+			}
+		}
+
+		printf("입력한 단어: %s, 생략한 단어: %s ", str, strcp);
+
+	
+	}
+
+	return 0;
+}
