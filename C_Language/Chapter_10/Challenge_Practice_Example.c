9d54eb3a4695027d2772f54114e5bbc76dfa3a29
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-05-17 20:26:57 +0900

    2020.05.17_Study array and pointer

---
+++ C_Language/Chapter_10/Challenge_Practice_Example.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+// p.292
+// 로또 번호 생성 프로그램
+
+// 배열에 로또 번호를 입력하는 함수
+void input_nums(int* lotto_nums);
+
+// 배열에 저장된 값을 출력하는 함수
+void print_nums(int* lotto_nums);
+
+
+int main(void)
+{
+	// 로또 번호를 저장할 배열
+	int lotto_nums[6] = {0};
+
+	// 입력 함수 호출
+	input_nums(lotto_nums);
+
+	// 출력 함수 호출
+	print_nums(lotto_nums);
+	
+	return 0;
+}
+
+
+void input_nums(int* lotto_nums)
+{
+	int num = 0;
+	int cnt = 0;
+
+	while (1) {
+
+		printf("번호 입력:");
+		scanf_s("%d", &num);
+
+		for (int i = 0; i < 6; i++) {
+
+			if (lotto_nums[i] == num) {
+			
+				printf("같은 번호가 있습니다!\n");
+				break;
+
+			}
+			else if (lotto_nums[i] == 0) {
+
+				lotto_nums[i] = num;
+				cnt++;
+				break;
+			
+			}
+		}
+
+		if (cnt == 6) {
+			break;
+		}
+	}
+
+}
+
+void print_nums(int* lotto_nums)
+{
+	printf("로또 번호: ");
+	for (int i = 0; i < 6; i++) 
+	{
+		printf("%d\t", lotto_nums[i]);
+	}
+}
---
+++ C_Language/Chapter_10/CheckUp_Problem_02.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// p.291 
+
+void print_month(int *);
+
+int main(void) 
+{
+	int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	print_month(month);
+
+	return 0;
+
+}
+
+void print_month(int *mp)
+{
+	int i;
+	int cnt = 0;
+	for (i = 0; i <12; i++)
+	{
+		printf("%5d", mp[i]);
+		cnt++;
+		if ((cnt >= 5) && (cnt % 5 == 0))
+		{
+			printf("\n");
+
+		}
+	
+	}
+}
+
+
+
