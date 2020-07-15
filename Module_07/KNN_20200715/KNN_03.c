94a2e3452f9e9179a0ab79a5878a7335ea6fe110
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-07-16 06:04:50 +0900

    Update KNN_03.c

--- Module_07/KNN_20200715/KNN_03.c
+++ Module_07/KNN_20200715/KNN_03.c
@@ -6,17 +6,26 @@ unsigned char train_image[28 * 28 * 10000];
 unsigned char test_label[10000];
 unsigned char train_label[10000];
 
-double dist[10000 * 10000];
+//double dist[10000 * 10000];
 int minDistIdx[10000];
+unsigned char Lbs_minDistIdx[10000];
+
+
+unsigned char train_lbs_image[28 * 28 * 10000];
+unsigned char test_lbs_image[28 * 28 * 10000];
+
+
 int count = 0;
 double dist_temp = 0;
 int dist1, dist2;
 
+
+// 함수
 void getDistAll();
 void getMinClass();
 void getRecoRate();
 int CalDist();
-
+int LBP_CalDist();
 
 
 int main()
@@ -55,6 +64,40 @@ int main()
     fclose(train_idx);
 
 
+
+    // LBP
+    for (int k = 0; k < 10000; k++) {
+        for (int j = 1; j < 27; j++) {
+            for (int i = 1; i < 27; i++) {
+
+                int m = 0;
+                unsigned char val1 = 0;
+                unsigned char val2 = 0;
+
+                for (int sj = -1; sj < 2; sj++) {
+                    for (int si = -1; si < 2; si++) {
+                        if (si != 0 && sj != 0) {
+                            if (train_image[k * 28 * 28 + j * 28 + i] <= train_image[k * 28 * 28 + (j + sj) * 28 + i + si])
+                                val1 |= (1 << m);
+                            if (test_image[k * 28 * 28 + j * 28 + i] <= test_image[k * 28 * 28 + (j + sj) * 28 + i + si])
+                                val2 |= (1 << m);
+                        }
+                        else if (si == 0 && sj == 0)
+                        {
+                            m--;
+                        }
+                        m++;
+                    }
+                }
+
+                train_lbs_image[k * 28 * 28 + j * 28 + i] = val1;
+                test_lbs_image[k * 28 * 28 + j * 28 + i] = val2;
+            }
+        }
+    }
+
+
+
     // 함수 호출
     getMinClass();
     getRecoRate();
@@ -72,60 +115,41 @@ int CalDist(int test_idx, int train_idx) {
 
 }
 
-void getDistAll()
-{
 
-    int m = 0;
-    int n = 0;
-    int v = 0;
-    int num = 28 * 28;
-
-    for (m = 0; m < 10000; m++)
-    {
-
-        for (n = 0; n < 10000; n++)
-        {
-            for (v = 0; v < 28 * 28; v++) {
-                dist_temp += ((int)(test_image[m * num + v] - train_image[n * num + v])) * ((int)(test_image[m * num + v] - train_image[n * num + v]));
-                dist[m * 10000 + n] = sqrt(dist_temp);
-            }
-
-
-            //dist[m * 10000 + n] = sqrt(dist_temp);
-
-        }
-
-        printf_s("%lf\n", dist[m * 10000 + n]);
-        //printf_s("%d\n", m);
+int LBP_CalDist(int test_idx, int train_idx) {
+    for (int j = 0; j < 28 * 28; j++) {
+        dist2 += (test_lbs_image[test_idx + j] - train_lbs_image[train_idx + j]) * (test_lbs_image[test_idx + j] - train_lbs_image[train_idx + j]);
     }
+    return sqrt(dist2);
+
 }
+
 
 
 void getMinClass() {
 
     double temp = 0;
     double minDist = 1e6;
-    //double minDistArr[10000];
-    //struct idxDist minDistArr[10000] ;
-
-
-    //getDistAll();
 
 
     for (int k = 0; k < 10000; k++) // 학습 데이터
     {
-       // dist1 = CalDist(k * 28 * 28, j * 28 * 28);
         minDist = 1e6;
         for (int j = 0; j < 10000; j++) // 테스트 데이터
         {
             dist1 = CalDist(k * 28 * 28, j * 28 * 28);
-           // temp = dist[k * 10000 + j];
+            dist2 = LBP_CalDist(k * 28 * 28, j * 28 * 28);
             if (minDist > dist1)
             {
                 minDist = dist1;
-                //minDistArr[count] = temp;
                 minDistIdx[k] = train_label[j]; // 최소 거리인 클래스 저장
-                //count++;
+            }
+
+            if (minDist > dist2)
+            {
+                minDist = dist2;
+                Lbs_minDistIdx[k] = train_label[j]; // 최소 거리인 클래스 저장
+
             }
         }
 
@@ -134,54 +158,23 @@ void getMinClass() {
     }
 }
 
-
-//int sorting()
-//{
-//   double minDistArr[10000] = { 0 };
-//   double temp = 0.0;
-//
-//
-//   
-//   *minDistArr = getMinClass();
-//
-//   for (int i = 0; i < count; i++) {
-//      for (int j = 0; j < count - i - 1; j++) {
-//         if (minDistArr[j] > minDistArr[j + 1]) {
-//            temp = minDistArr[j];
-//            minDistArr[j] = minDistArr[j + 1];
-//            minDistArr[j + 1] = temp;
-//         }
-//      }
-//   }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//   return minDistArr;
-//   
-//}
-//
-//
-
-
 void getRecoRate()
 {
-    //double recoRate = 0;
-    int recoRate = 0;
+    int recoRate1 = 0;
+    int recoRate2 = 0;
 
     for (int i = 0; i < 10000; i++)
     {
         if (test_label[i] == minDistIdx[i]) {
-            recoRate++;
+            recoRate1++;
+        }
+
+        if (test_label[i] == Lbs_minDistIdx[i]) {
+            recoRate2++;
         }
     }
 
-
     //printf_s("정답률 : %lf", recoRate);
-    printf_s("정답률 : %lf", (double)recoRate / 10000 * 100);
+    printf_s("K-NN 정답률 : %lf", (double)recoRate1 / 10000 * 100);
+    printf_s("LBP 정답률 : %lf", (double)recoRate2 / 10000 * 100);
 }
